4c57aa93b9a1df3bb4eec9448fec4a11f70c6c36
Thang PC home <duongnhatthang@gmail.com>
2024-04-23 12:47:59 -0700

    bug plotting when refactor code

--- evaluation.py
+++ evaluation.py
@@ -4,8 +4,10 @@ from model import UCB, TS, PHE
 from data import generate_contexts
 import time, json, itertools
 
-# def _eval_one_sim(model, beta, n_gen_context, d, T, sim_idx, elapsed_time, beta_err, rho=0.5, noise_std=1, seed=0):
-def _eval_one_sim(input_dict, model,beta, sim_idx, elapsed_time, beta_err):
+MODE_RANDOM = 0
+MODE_ADVERSARY = 1
+
+def _eval_one_sim(input_dict, model,theta, sim_idx, elapsed_time, theta_err):
     n_gen_context = input_dict["n_gen_context"]
     T = input_dict["T"]
     d = input_dict["d"]
@@ -17,20 +19,21 @@ def _eval_one_sim(input_dict, model,beta, sim_idx, elapsed_time, beta_err):
 
     for t in range(T):
         # generate contexts
-        contexts = generate_contexts(n_gen_context, d, rho, seed=seed+t+sim_idx)
+        if seed is not None:
+            seed=seed+t+sim_idx
+        contexts = generate_contexts(n_gen_context, d, rho, seed=seed)
         # optimal reward
-        opt_reward.append(np.amax(np.array(contexts) @ beta))
+        opt_reward.append(np.amax(np.array(contexts) @ theta))
         # time
         start = time.time()
         a_t = model.select_ac(contexts)
-        reward = np.dot(contexts[a_t],beta) + np.random.normal(0, noise_std, size=1)
-        model_reward.append(np.dot(contexts[a_t],beta))
+        reward = np.dot(contexts[a_t],theta) + np.random.normal(0, noise_std, size=1)
+        model_reward.append(np.dot(contexts[a_t],theta))
         model.update(reward)
         elapsed_time[sim_idx,t] = time.time() - start
-        beta_err[sim_idx,t] = np.linalg.norm(model.beta_hat-beta)
+        theta_err[sim_idx,t] = np.linalg.norm(model.theta_hat-theta)
     return opt_reward, model_reward
 
-# def _post_process(output, results, n_gen_context, d, name):
 def _post_process(input_dict, results):
     output = input_dict["output"]
     d = input_dict["d"]
@@ -39,7 +42,7 @@ def _post_process(input_dict, results):
     if output:
         # Plotting
         last_regret = []
-        for result in results:
+        for result in results: #Iters over different params
             if name == 'TS':
                 v = result['settings']['v']
             else:
@@ -63,7 +66,7 @@ def eval(input_dict):
     results = []
     for param in params_set:
         cumul_regret = np.zeros((n_sim,T))
-        beta_err = np.zeros((n_sim,T))
+        theta_err = np.zeros((n_sim,T))
         elapsed_time = np.zeros((n_sim,T))
         for sim_idx in range(n_sim):
             print('%s Simulation %d, N=%d, d=%d, alpha=%.3f' % (name, sim_idx+1, n_gen_context, d, param))
@@ -74,24 +77,50 @@ def eval(input_dict):
                 model = TS(d=d, v=param)
             elif name=="PHE":
                 model = PHE(d=d, alpha=param)
-            np.random.seed(seed+sim_idx)
-            # true beta
-            beta = np.random.uniform(-1/np.sqrt(d),1/np.sqrt(d),d) #ensure unit ball length
-            opt_reward, model_reward = _eval_one_sim(input_dict, model, beta, sim_idx, elapsed_time, beta_err)
+
+            if seed is not None:
+                np.random.seed(seed+sim_idx)
+            # true theta
+            theta = np.random.uniform(-1/np.sqrt(d),1/np.sqrt(d),d) #ensure unit ball length
+            opt_reward, model_reward = _eval_one_sim(input_dict, model, theta, sim_idx, elapsed_time, theta_err)
 
             cumul_regret[sim_idx,:] = np.cumsum(opt_reward)-np.cumsum(model_reward)
         ##Save at dict
         results.append({'model':name,
                         'settings':model.settings,
                         'regrets':cumul_regret.tolist(),
-                        'beta_err':beta_err.tolist(),
+                        'theta_err':theta_err.tolist(),
                         'time':elapsed_time.tolist()})
     return _post_process(input_dict, results)
 
+# def _post_process_multi(input_dict, results):
+#     output = input_dict["output"]
+#     d = input_dict["d"]
+#     name = input_dict["name"]
+#     n_gen_context = input_dict["n_gen_context"]
+#     if output:
+#         # Plotting
+#         last_regret = []
+#         for result in results: #Iters over different params
+#             if name == 'TS':
+#                 v = result['settings']['v']
+#             else:
+#                 alpha = result['settings']['alpha']
+#             last_regret.append(np.mean(result['regrets'], axis=0)[-1])
+#         if np.argmin(last_regret) >= len(results) or np.argmin(last_regret)<0:
+#             import pdb; pdb.set_trace()
+#         best = results[np.argmin(last_regret)]
+#         return best
+#     else:
+#         # Save to txt file
+#         with open('./results/{%s}_d%d_N%d.txt' % (name, d, n_gen_context), 'w+') as outfile:
+#             json.dump(results, outfile)
 
 from scipy.stats import ortho_group
+
+
 def eval_multi(input_dict):
-    #inputs: n_sim, n_gen_context, d, T, rho, seed, B(bound for the beta)
+    #inputs: n_sim, n_gen_context, d, T, rho, seed, B(bound for the theta)
     name = input_dict["name"]
     n_gen_context = input_dict["n_gen_context"]
     params_set = input_dict["params_set"]
@@ -106,9 +135,9 @@ def eval_multi(input_dict):
     B = np.array(B)[:,:m]
     results = []
     for param in params_set:
-        cumul_regret = np.zeros((n_sim,T,n_task))
-        beta_err = np.zeros((n_sim,T,n_task))
-        elapsed_time = np.zeros((n_sim,T,n_task))
+        cumul_regret_all = np.zeros((n_sim,T,n_task))
+        theta_err_all = np.zeros((n_sim,T,n_task))
+        elapsed_time_all = np.zeros((n_sim,T,n_task))
         for sim_idx in range(n_sim):
             print('%s Simulation %d, N_gen_ctx=%d, d=%d, alpha=%.3f' % (name, sim_idx+1, n_gen_context, d, param))
             # call model
@@ -118,22 +147,65 @@ def eval_multi(input_dict):
                 model = TS(d=d, v=param)
             elif name=="PHE":
                 model = PHE(d=d, alpha=param)
-            np.random.seed(seed+sim_idx)
+
+            if seed is not None:
+                np.random.seed(seed+sim_idx)
+            n_revealed=1
             for task_idx in trange(n_task):
-                beta_err_i = beta_err[:,:,task_idx]
-                elapsed_time_i = elapsed_time[:,:,task_idx]
-                # true beta
-                w_i = np.random.uniform(-1,1,m)
-                u = np.random.uniform(0,1) #Scaling factor
-                beta = B @ w_i
-                beta = u*beta/np.linalg.norm(beta) #ensure unit ball length
-                opt_reward, model_reward = _eval_one_sim(input_dict, model, beta, sim_idx, elapsed_time_i, beta_err_i)
-                cumul_regret[sim_idx,:,task_idx] = np.cumsum(opt_reward)-np.cumsum(model_reward)
+                theta_err_i = theta_err_all[:,:,task_idx]
+                elapsed_time_i = elapsed_time_all[:,:,task_idx]
+                # true theta
+                # w_i = np.random.uniform(-1,1,m)
+                # u = np.random.uniform(0,1) #Scaling factor
+                # theta = B @ w_i
+                # theta = u*theta/np.linalg.norm(theta) #ensure unit ball length
+                theta = gen_params(B, input_dict, task_idx, n_revealed)
+                print(f"n_revealed = {n_revealed}, theta={theta}")
+                opt_reward, model_reward = _eval_one_sim(input_dict, model, theta, sim_idx, elapsed_time_i, theta_err_i)
+                cumul_regret_all[sim_idx,:,task_idx] = np.cumsum(opt_reward)-np.cumsum(model_reward)
                 model.reset()
         ##Save at dict
         results.append({'model':name,
                         'settings':model.settings,
-                        'regrets':cumul_regret,
-                        'beta_err':beta_err,
-                        'time':elapsed_time})
+                        'regrets_all':cumul_regret_all,
+                        'theta_err_all':theta_err_all,
+                        'time_all':elapsed_time_all,
+                        'regrets':cumul_regret_all.mean(axis=1),
+                        'theta_err':theta_err_all.mean(axis=1),
+                        'time':elapsed_time_all.mean(axis=1)})
     return _post_process(input_dict, results)
+
+def gen_params(B, input_dict, task_idx, n_revealed):
+    def _gen_params_from_B(B, m):
+        w_i = np.random.uniform(-1,1,m)
+        u = np.random.uniform(0,1) #Scaling factor
+        print(f"Scaling u = {u}")
+        theta = B @ w_i
+        theta = u*theta/np.linalg.norm(theta) #ensure unit ball length
+        return theta
+
+    T = input_dict["T"]
+    d = input_dict["d"]
+    m = input_dict["m"]
+    n_task = input_dict["n_task"]
+    adv_const = input_dict["adv_const"]
+    mode = input_dict["mode"]
+    if mode == MODE_RANDOM:
+        theta = _gen_params_from_B(B, m)
+    elif mode == MODE_ADVERSARY:
+        q = adv_const*(d-m)/((np.sqrt(T)-m)*(1+np.sqrt(2*(n_task-task_idx-1)))) #probability of revealing a new dimension
+        print(f"q={q}")
+        reveal_new = np.random.binomial(n=1, p=q)
+        if reveal_new:
+            n_revealed = min(n_revealed+1,m)
+        # diag = np.random.uniform(-1,1,m)
+        # diag[n_revealed:] = 0
+        # print(f"diag = {diag}")
+        # diag = np.diagflat(diag)
+        # import pdb; pdb.set_trace()
+        # low_rank_B = (B @ diag) @ B.T
+        # low_rank_B = np.array(low_rank_B)[:,:m]
+        low_rank_B = np.copy(B)
+        low_rank_B[:, n_revealed:] = 0
+        theta = _gen_params_from_B(low_rank_B, m)
+    return theta
